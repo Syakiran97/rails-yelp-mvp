6f7bb7683e6ee37b33df5223a0aa1fea778c4d90
Muhammad Syakiran <syakiranmuhd@gmail.com>
2020-11-25 15:39:18 +0800

    completed challenge 2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,23 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
-  @review = Review.new(review_params)
-  @review.save
-
-    redirect_to review_path(@review)
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
-  def restaurant_params
+  def review_params
     params.require(:review).permit(:content, :rating)
   end
 end
